5d358da4704b66645709f29b78665b6e53bc7e68
UKASHA09 <UKASHA09@users.noreply.github.com>
2025-12-05 12:23:12 +0530

    dates

--- components/About/About.js
+++ components/About/About.js
@@ -31,11 +31,11 @@ const About = () => {
             </div>
             <div className={styles.date_container}>
                 <div className={styles.left_date} >
-                    <Date day="28" month="October" subtxt="th" />
+                    <Date day="14" month="January" subtxt="th" />
                 </div>
                 <hr className={styles.line} />
                 <div className={styles.right_date} >
-                    <Date day="31" month="October" subtxt="st" />
+                    <Date day="17" month="January" subtxt="st" />
                 </div>
             </div>
             <br />
