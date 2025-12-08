7ea03d4b7a9d9f6796e3c2b33940459430e7d411
UKASHA09 <UKASHA09@users.noreply.github.com>
2025-12-08 12:28:38 +0530

    team update fix

--- components/Team/Team.js
+++ components/Team/Team.js
@@ -20,122 +20,122 @@ export const Team = () => {
         })
     }, [])
     return (
-        <div className={styles.team_container}>
-            <h1>
-                Our <span>Team</span>
-            </h1>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Mentors
-            </h2>
-            <div className={styles.mentor}>
-                {mentors.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Developers
-            </h2>
-            <div className={styles.cards}>
-                {developers.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Event Managers
-            </h2>
-            <div className={styles.cards}>
-            {eventM.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Organisers
-            </h2>
-            <div className={styles.cards}>
-            {organisers.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Design Team
-            </h2>
-            <div className={styles.cards}>
-            {design.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Content Team
-            </h2>
-            <div className={styles.cards}>
-            {content.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-            <h2 data-aos="zoom-in" data-aos-duration="1500">
-                Outreach Team
-            </h2>
-            <div className={styles.cards}>
-            {outreach.map((data) => (
-                    <Card
-                        key={data.id}
-                        name={data.name}
-                        gif={data.gif}
-                        image={data.image}
-                        linkedin={data.linkedin}
-                        instagram={data.instagram}
-                        twitter={data.twitter}
-                    />
-                ))}
-            </div>
-        </div>
+        // <div className={styles.team_container}>
+        //     <h1>
+        //         Our <span>Team</span>
+        //     </h1>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Mentors
+        //     </h2>
+        //     <div className={styles.mentor}>
+        //         {mentors.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Developers
+        //     </h2>
+        //     <div className={styles.cards}>
+        //         {developers.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Event Managers
+        //     </h2>
+        //     <div className={styles.cards}>
+        //     {eventM.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Organisers
+        //     </h2>
+        //     <div className={styles.cards}>
+        //     {organisers.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Design Team
+        //     </h2>
+        //     <div className={styles.cards}>
+        //     {design.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Content Team
+        //     </h2>
+        //     <div className={styles.cards}>
+        //     {content.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        //     <h2 data-aos="zoom-in" data-aos-duration="1500">
+        //         Outreach Team
+        //     </h2>
+        //     <div className={styles.cards}>
+        //     {outreach.map((data) => (
+        //             <Card
+        //                 key={data.id}
+        //                 name={data.name}
+        //                 gif={data.gif}
+        //                 image={data.image}
+        //                 linkedin={data.linkedin}
+        //                 instagram={data.instagram}
+        //                 twitter={data.twitter}
+        //             />
+        //         ))}
+        //     </div>
+        // </div>
     )
 }
--- data/team.js
+++ data/team.js
@@ -1,31 +1,31 @@
-const mentors = [
-    {
-        id: 1,
-        name: 'Prof Ahtesham ',
-        image: 'https://res.cloudinary.com/dlu72yo9b/image/upload/v1633973233/WhatsApp_Image_2021-10-10_at_7.26.40_PM_km3wsd.jpg',
-        gif: 'https://c.tenor.com/DHGvsLhTOowAAAAC/meme-pikachu.gif',
-    },
-    // {
-    //     id: 2,
-    //     name: 'Hrishabh Joshi',
-    //     image: 'https://res.cloudinary.com/dlu72yo9b/image/upload/v1632027220/IMG-20210825-WA0061_3_rtjas0.jpg',
-    //     gif: 'https://c.tenor.com/xEu4tiLm3hUAAAAM/mr-bean-mr-beans-holiday.gif',
-    //     linkedin: 'https://www.linkedin.com/in/hrishabh-joshi/',
-    //     instagram: 'https://www.instagram.com/_hrishabh_08/',
-    //     twitter: 'https://twitter.com/hrishabhjoshi08',
-    // },
-]
-
-// const developers = [
+// const mentors = [
 //     {
 //         id: 1,
-//         name: 'Ukasha Siddiqui',
-//         image: 'https://res.cloudinary.com/hackbot/image/upload/v1633927567/profile1_ogifvj.png',
-//         gif: 'https://c.tenor.com/xPOkmiPmDT8AAAAM/bhagam-bhag-akshay-kumar-naughty.gif',
-//         linkedin: 'https://linkedin.com/in/yadav-aryan',
-//         instagram: 'https://instagram.com/aryann__7',
+//         name: 'Prof Ahtesham ',
+//         image: 'https://res.cloudinary.com/dlu72yo9b/image/upload/v1633973233/WhatsApp_Image_2021-10-10_at_7.26.40_PM_km3wsd.jpg',
+//         gif: 'https://c.tenor.com/DHGvsLhTOowAAAAC/meme-pikachu.gif',
 //     },
+//     // {
+//     //     id: 2,
+//     //     name: 'Hrishabh Joshi',
+//     //     image: 'https://res.cloudinary.com/dlu72yo9b/image/upload/v1632027220/IMG-20210825-WA0061_3_rtjas0.jpg',
+//     //     gif: 'https://c.tenor.com/xEu4tiLm3hUAAAAM/mr-bean-mr-beans-holiday.gif',
+//     //     linkedin: 'https://www.linkedin.com/in/hrishabh-joshi/',
+//     //     instagram: 'https://www.instagram.com/_hrishabh_08/',
+//     //     twitter: 'https://twitter.com/hrishabhjoshi08',
+//     // },
 // ]
+
+// // const developers = [
+// //     {
+// //         id: 1,
+// //         name: 'Ukasha Siddiqui',
+// //         image: 'https://res.cloudinary.com/hackbot/image/upload/v1633927567/profile1_ogifvj.png',
+// //         gif: 'https://c.tenor.com/xPOkmiPmDT8AAAAM/bhagam-bhag-akshay-kumar-naughty.gif',
+// //         linkedin: 'https://linkedin.com/in/yadav-aryan',
+// //         instagram: 'https://instagram.com/aryann__7',
+// //     },
+// // ]
 
 // const organisers = [
 //     {
