bc54a49f3ad63a90e7e4fbfc7eb77012a5274d59
UKASHA09 <UKASHA09@users.noreply.github.com>
2025-12-08 12:12:51 +0530

    updated

--- data/timeline.js
+++ data/timeline.js
@@ -67,19 +67,6 @@ const event = [
 
     {
         day: 4,
-        name: 'Take De Bait!',
-        time: '9:00 AM - 11:30 AM',
-        link: 'take-de-bait',
-    },
-    {
-        day: 4,
-        name: 'Rajen Dasgupta’s Industrial Robotics Introduction',
-        time: '12:00 PM - 2:00 PM',
-        link: 'https://us06web.zoom.us/j/88332504978?pwd=THBLcTJWWmg0UUhIYlNNR3FiN0pPZz09',
-    },
-    // Day4
-    {
-        day: 4,
         name: 'Event',
         time: '00:00 AM - 00:00 AM',
     },
