9dc2b13091757029ae367a833ffde6b829bc4ccb
UKASHA09 <UKASHA09@users.noreply.github.com>
2025-12-28 23:41:14 +0530

    Update RightNav.jsx

--- components/Nav/RightNav.jsx
+++ components/Nav/RightNav.jsx
@@ -102,7 +102,7 @@ const RightNav = ({ open, setOpen }) => {
                     </Link>
                 </li>
                 <li onClick={toggle}>
-                    <Link href="#" passHref>
+                    <Link href="/#Team" passHref>
                         Team
                     </Link>
                 </li>
