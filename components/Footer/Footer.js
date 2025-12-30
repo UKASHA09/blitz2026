b7ebeb55f05b2adfa75b971b3ecfc3595713d78a
UKASHA09 <UKASHA09@users.noreply.github.com>
2025-12-30 18:26:59 +0530

    upperfooterremoved

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -17,63 +17,7 @@ const Footer = () => {
     }
     return (
         <div className={styles.footer}>
-            <div className={styles.footerUpper}>
-                <div className={styles.logo}>
-                    <Image src={logo} alt="Designed By Ukasha" priority={true} />
-                </div>
-                <div className={styles.icons}>
-                    <ul>
-                        <li>
-                            <Link
-                                href="mailto:blitzforum20@gmail.com"
-                                passHref={true}
-                            >
-                                <FaEnvelope />
-                            </Link>
-                        </li>
-                        {/* <li>
-                            <Link
-                                href="https://www.facebook.com/"
-                                passHref={true}
-                            >
-                                <a target="_blank">
-                                    <FaFacebookSquare />
-                                </a>
-                            </Link>
-                        </li> */}
-                        <li>
-                            <Link
-                                href="https://www.instagram.com/blitzunleashed"
-                                passHref={true}
-                            >
-                                <a target="_blank">
-                                    <FaInstagram />
-                                </a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="https://chat.whatsapp.com/IthIj4yfl1U8v2o8EWSq82"
-                                passHref={true}
-                            >
-                                <a target="_blank">
-                                    <SiWhatsapp />
-                                </a>
-                            </Link>
-                        </li>
-                        {/* <li>
-                            <Link
-                                href="https://www.youtube.com/channel/"
-                                passHref={true}
-                            >
-                                <a target="_blank">
-                                    <FaYoutube />
-                                </a>
-                            </Link>
-                        </li> */}
-                    </ul>
-                </div>
-            </div>
+            {/* cut from here */}
             <div className={styles.footerLower}>
                 <span onClick={love} className={styles.heart}>Developed by Ukasha Siddiqui, FYIT, APCCE</span>
             </div>
